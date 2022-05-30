71d3dbb66703b31a027ec9479f6fdcb32e725ff7
vaka <vadym.ponomarov@estcube.eu>
2022-05-30 21:26:55 +0300

    DynamoDB setup and first POST function

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,15 +1,21 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
+import { v4 } from "uuid";
 
-export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const reqBody = JSON.parse(event.body as string);
+  const product = { ...reqBody, productID: v4() };
+
+  await docClient
+    .put({
+      TableName: "ProductsTable",
+      Item: product,
+    })
+    .promise();
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2,
-    ),
+    body: JSON.stringify(product),
   };
 };
